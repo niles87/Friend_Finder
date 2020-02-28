68e29f8b7e5469306176a91a65b5d1c293f67da7
niles87 <nbing19@gmail.com>
2020-02-27 19:43:31 -0800

    added friend data

---
+++ app/data/friends.js
@@ -0,0 +1,10 @@
+var potentials = [
+  {
+    name: "Daniel",
+    photo:
+      "https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    scores: [3, 5, 5, 1, 3, 5, 5, 2, 1, 2],
+  },
+];
+
+module.exports = potentials;
