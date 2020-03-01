a96769295daf303879938b26f8ed175042197b62
niles87 <nbing19@gmail.com>
2020-02-29 17:56:52 -0800

    added more to the body of the callback function to handle the post request and respond with the best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,12 +6,31 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-    var newFriend = req.body;
+    var userInfo = req.body;
 
-    console.log(newFriend);
+    console.log(userInfo);
 
-    potentials.push(newFriend);
+    var userAnswers = userInfo.scores;
 
-    res.json(newFriend);
+    var friendName = "";
+    var friendPhoto = "";
+
+    var totalDifference = 100;
+
+    for (var i = 0; i < potentials.length; i++) {
+      var difference = 0;
+      for (var j = 0; j < userAnswers.length; j++) {
+        difference += Math.abs(potentials[i].scores[j] - userAnswers[j]);
+      }
+      if (difference < totalDifference) {
+        totalDifference = difference;
+        friendName = potentials[i].name;
+        friendPhoto = potentials[i].photo;
+      }
+    }
+
+    potentials.push(userInfo);
+
+    res.json({ status: "OK", friendName: friendName, friendPhoto: friendPhoto });
   });
 };
