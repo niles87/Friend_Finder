a9f10ce7efa3b57608038745c66f50c84b251cd5
niles87 <nbing19@gmail.com>
2020-02-29 17:50:57 -0800

    added more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,18 @@ var potentials = [
       "https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
     scores: [3, 5, 5, 1, 3, 5, 5, 2, 1, 2],
   },
+  {
+    name: "Ashley",
+    photo:
+      "https://images.pexels.com/photos/372042/pexels-photo-372042.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [5, 2, 3, 5, 3, 3, 5, 2, 1, 2],
+  },
+  {
+    name: "Jake",
+    photo:
+      "https://images.pexels.com/photos/594610/pexels-photo-594610.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores: [2, 1, 5, 1, 3, 1, 1, 2, 1, 2],
+  },
 ];
 
 module.exports = potentials;
